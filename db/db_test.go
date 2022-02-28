b8acf4a6c56b983b87c8cbaf2cc61a891974c2c8
un-versed <gu.bat98@gmail.com>
2022-02-28 18:21:35 -0300

    test: db tests

---
+++ db/db_test.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"flag"
+	"reflect"
+	"testing"
+
+	"github.com/un-versed/base_api/helpers"
+)
+
+var pgConnString = flag.String("pg", helpers.GetDefaultDatabaseConnectionString(), "Postgres connection string")
+
+func TestOpen(t *testing.T) {
+	err := Open(*pgConnString)
+	if err != nil {
+		t.Errorf("Unable to connect to database: %s\n", err.Error())
+	}
+
+	err = Open("whatever")
+	if err == nil {
+		t.Errorf("Connecting with wrong strin")
+	}
+}
+
+func TestConn(t *testing.T) {
+	conn := Conn()
+	if reflect.TypeOf(conn).String() != "*bun.DB" {
+		t.Errorf("Unable to create to connection pointer.")
+	}
+}
+
+func TestClose(t *testing.T) {
+	Close()
+}
