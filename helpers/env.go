6da3747c62e885fb1700a6967cc4d411445657ff
un-versed <gu.bat98@gmail.com>
2022-03-02 15:16:44 -0300

    fix: remove database connection string helpers

--- helpers/env.go
+++ helpers/env.go
@@ -5,16 +5,6 @@ import (
 	"strconv"
 )
 
-func GetDefaultDatabaseConnectionString() string {
-	res := "postgres://postgres:postgres@localhost:5432/postgres"
-	env := os.Getenv("DATABASE_URL")
-	if len(env) > 0 {
-		res = env
-	}
-
-	return res
-}
-
 func GetDefaultPort() int {
 	res := 8080
 	env := os.Getenv("SERVER_PORT")
--- helpers/env_test.go
+++ helpers/env_test.go
@@ -6,22 +6,6 @@ import (
 	"testing"
 )
 
-func TestGetDefaultDatabaseConnectionString(t *testing.T) {
-	defaultString := "postgres://postgres:postgres@localhost:5432/postgres"
-	customString := "postgres://test:test@localhost:5432/postgres"
-	os.Setenv("DATABASE_URL", "")
-	connString := GetDefaultDatabaseConnectionString()
-	if connString != defaultString {
-		t.Errorf("Expected GetDefaultDatabaseConnectionString() to be equal %s. Got %s", defaultString, connString)
-	}
-
-	os.Setenv("DATABASE_URL", customString)
-	connString = GetDefaultDatabaseConnectionString()
-	if connString != customString {
-		t.Errorf("Expected GetDefaultDatabaseConnectionString() to be equal %s. Got %s", defaultString, connString)
-	}
-}
-
 func TestGetDefaultPort(t *testing.T) {
 	defaultPort := 8080
 	customPort := 3000
